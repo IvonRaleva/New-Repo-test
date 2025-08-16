afbf99fdb0c7f8d945ffb9424b0c742ca28f62ec
IvonRaleva <ivonraleva@gmail.com>
2025-08-16 14:48:04 +0300

    new file

---
+++ StoryDTO.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Story.Models
+{
+    internal class StoryDTO
+    {
+        [JsonPropertyName("name")]
+        public string? Name { get; set; }
+
+        [JsonPropertyName("description")]
+        public string? Description { get; set; }
+
+        [JsonPropertyName("url")]
+        public string? Url { get; set; }
+    }
+}
+
